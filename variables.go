f8e4377d2263dd42e28fc1bdc871a87658224401
fedemengo <mengozzi.federico@gmail.com>
2018-09-02 22:23:08 +0200

    Add proper comments to exported variables

--- variables.go
+++ variables.go
@@ -4,8 +4,10 @@ import (
 	"github.com/bclicn/color"
 )
 
+// PathColored set the path color
 func PathColored(path string) string { return color.Blue(path) }
 
+// Message set the color and message for each symbol
 var Message = map[byte]string{
 	'A': color.Green("ADDED"),
 	'D': color.Red("DELETED"),
@@ -16,10 +18,14 @@ var Message = map[byte]string{
 	'-': color.Green("CLEAN"),
 }
 
+// Location set the color of repository Index and Working Tree
 var Location = [...]string{
 	color.LightCyan("Index"),
 	color.LightCyan("Working Tree"),
 }
 
+// ErrorSymbol set the color for error
 var ErrorSymbol = color.Red("X")
+
+// SkipDir set the color for skipped directories
 var SkipDir = color.Cyan("Directory to SKIP:")
